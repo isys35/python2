7d4032e0ebe9d570e508a53a1fd590da75695a63
isys35 <isys35@mail.ru>
2021-05-20 12:18:53 +0300

    add room_class field in model Room

--- hotel/models.py
+++ hotel/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 
 class Room(models.Model):
+    ROOM_CLASS = [
+        ('ECN', 'Эконом'),
+        ('STD', 'Стандарт'),
+        ('VIP', 'VIP'),
+    ]
     number = models.PositiveIntegerField(unique=True, verbose_name="Номер комнаты")
     floor = models.PositiveIntegerField(verbose_name="Этаж")
-    number_of_rooms = models.PositiveIntegerField(verbose_name='Кол-во комнат')
+    number_of_rooms = models.PositiveIntegerField(verbose_name='Кол-во комнат', db_index=True)
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
+    room_class = models.CharField(max_length=10, choices=ROOM_CLASS, db_index=True)
