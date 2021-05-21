da13eee45fdc92ee4b4da104b261a74795de4973
isys35 <isys35@mail.ru>
2021-05-21 16:07:09 +0300

    add template create_room.html add form template room_form.html add urls change and add room in index.html and room.html add RoomForm add class views to change and add Room

--- hotel/urls.py
+++ hotel/urls.py
@@ -5,6 +5,8 @@ from hotel import views
 app_name = "hotel"
 
 urlpatterns = [
+    path('room/create/', views.RoomCreateView.as_view(), name='add'),
+    path('room/edit/<int:pk>', views.RoomEditView.as_view(), name='edit'),
     path("room/<int:pk>", views.room_detail, name='detail'),
-    path("", views.main_page),
+    path("", views.main_page, name='main'),
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView, CreateView
+
+
+from .forms import RoomForm
 from .models import Room
 
 
@@ -12,3 +17,18 @@ def room_detail(request, pk):
     room = Room.objects.get(pk=pk)
     context = {'room': room}
     return render(request, 'hotel/room.html', context=context)
+
+
+class RoomEditView(UpdateView):
+    model = Room
+    form_class = RoomForm
+
+    def get_success_url(self):
+        return reverse_lazy('hotel:detail',
+                            kwargs={'pk': self.object.pk})
+
+
+class RoomCreateView(CreateView):
+    template_name = 'hotel/create_room.html'
+    form_class = RoomForm
+    success_url = reverse_lazy('hotel:main')
