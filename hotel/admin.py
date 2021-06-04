664313bf80340f8c5b8443aa5f882e5f85eecaee
isys35 <isys35@mail.ru>
2021-06-04 17:02:31 +0300

    hw

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Room, Reservation, CheckIn
+from .models import Room, Reservation, CheckIn, TypeService
+
+
+class TypeServiceAdmin(admin.ModelAdmin):
+    exclude = ("user",)
+    readonly_fields = ("avg_rate",)
 
 
 admin.site.register(Room)
 admin.site.register(Reservation)
 admin.site.register(CheckIn)
+admin.site.register(TypeService, TypeServiceAdmin)
--- hotel/models.py
+++ hotel/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models import Q
 
 
 class Room(models.Model):
@@ -19,6 +18,9 @@ class Room(models.Model):
     number_of_rooms = models.PositiveIntegerField(verbose_name='Кол-во комнат', choices=ROOMS_COUNT, db_index=True)
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
     room_class = models.CharField(max_length=10, choices=ROOM_CLASS, db_index=True, verbose_name='Класс номера')
+    user = models.ManyToManyField(User,
+                                  through="Reservation",
+                                  related_name="room")
 
     def __str__(self):
         return "Номер №{}".format(self.number)
@@ -47,3 +49,43 @@ class CheckIn(Reservation):
     class Meta:
         verbose_name_plural = 'Заселения'
         verbose_name = 'Заселение'
+
+
+class TypeService(models.Model):
+    title = models.CharField(max_length=50)
+    avg_rate = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+    user = models.ManyToManyField(
+        User,
+        related_name="rated_services",
+        blank=True
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Сервисы'
+        verbose_name = 'Сервис'
+
+
+class UserTypeService(models.Model):
+    class Meta:
+        unique_together = ("user", "type_service")
+
+    user = models.ForeignKey(
+        User,
+        related_name="rated_type_service",
+        on_delete=models.SET_DEFAULT,
+        default=3
+    )
+    type_service = models.ForeignKey(
+        TypeService,
+        on_delete=models.CASCADE,
+        related_name="rated_type_service"
+    )
+    rate = models.PositiveSmallIntegerField()
