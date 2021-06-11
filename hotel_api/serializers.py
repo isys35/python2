3fc22ff2cc02e8ea9efe0ac882880169c802f700
isys35 <isys35@mail.ru>
2021-06-11 11:12:25 +0300

    add serializer rezervation

--- hotel_api/serializers.py
+++ hotel_api/serializers.py
@@ -1,13 +1,7 @@
 from rest_framework import serializers
+from rest_framework.fields import CharField
 
-from hotel.models import Room, TypeService, UserTypeService
-
-
-class RoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Room
-        fields = ['id', 'number', 'floor', 'number_of_rooms',
-                  'description']
+from hotel.models import Room, TypeService, UserTypeService, Reservation
 
 
 class TypeServiceSerializer(serializers.ModelSerializer):
@@ -20,3 +14,20 @@ class UserTypeServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserTypeService
         fields = ['rate']
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    user = CharField(source="user.username")
+
+    class Meta:
+        model = Reservation
+        fields = ['user', 'description', 'started_at', 'ended_at']
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    booked = ReservationSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Room
+        fields = ['id', 'number', 'floor', 'number_of_rooms',
+                  'description', 'booked']
