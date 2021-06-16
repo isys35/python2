ae982600aa5e34afcf9688e5adcef441f026f38b
isys35 <isys35@mail.ru>
2021-06-16 11:59:47 +0300

    AJAX load typeservices

--- hotel/static/script.js
+++ hotel/static/script.js
@@ -39,29 +39,60 @@ var csrftoken = getCookie('csrftoken');
     })
 })*/
 
-function load_rooms() {
+function loadRooms() {
     let roomsContainer = $(".rooms-container");
     if (roomsContainer) {
         $.ajax({
             url: "hotel-api/rooms/",
             method: "GET",
             success: function (data) {
-                for (i=0; i<data.length; ++i) {
-                    var roomDom = "<div>\n" +
+                for (let i=0; i<data.length; ++i) {
+                    let roomHtml = "<div>\n" +
                     "   <div class=\"panel panel-default\">\n" +
                     `       <div class=\"panel-heading\"><a href=\"room/${data[i].id}\">Номер ${data[i].number}</a></div>\n` +
                     `       <div class=\"panel-body\"><a href=\"room/${data[i].id}\">${data[i].room_class} класс</a></div>\n` +
                     "   </div>\n" +
                     "</div>"
-                    roomsContainer.append(roomDom);
+                    roomsContainer.append(roomHtml);
                 }
             }
         });
     }
 }
 
-function load_page() {
-    load_rooms();
+function loadTypeServices() {
+    let typeServicesContainer = $(".type-services-container");
+     if (typeServicesContainer) {
+         $.ajax({
+            url: "hotel-api/services/",
+            method: "GET",
+            success: function (data) {
+                for (let i=0; i<data.length; ++i) {
+                    let servicesHtml = "" +
+                        "<div class=\"rate\">\n" +
+                        "    <div class=\"rate-title\">\n" +
+                        `         <span>${data[i].title}</span>\n` +
+                        "    </div>\n" +
+                        `        <a class=\"rate-btn\" id=\"typeservice_${data[i].id}_1\" href=\"#\"><span class=\"emoji\">😠</span></a>\n` +
+                        `         <a class=\"rate-btn\" id=\"typeservice_${data[i].id}_2\" href=\"#\"><span class=\"emoji\">😦</span></a>\n` +
+                        `         <a class=\"rate-btn\" id=\"typeservice_${data[i].id}_3\" href=\"#\"><span class=\"emoji\">😑</span></a>\n` +
+                        `         <a class=\"rate-btn\" id=\"typeservice_${data[i].id}_4\" href=\"#\"><span class=\"emoji\">😀</span></a>\n` +
+                        `         <a class=\"rate-btn\" id=\"typeservice_${data[i].id}_5\" href=\"#\"><span class=\"emoji\">😍</span></a>\n` +
+                        "         <div class=\"rate-avg\">\n" +
+                        `             <span id=\"rate-avg_${data[i].id}\">${data[i].avg_rate}</span>\n` +
+                        `             <span id=\"rate-avg-count_${data[i].id}\" class=\"rate-avg-count\">(${data[i].count_rate})</span>\n` +
+                        "         </div>\n" +
+                        "</div>"
+                    typeServicesContainer.append(servicesHtml);
+                }
+            }
+        });
+     }
 }
 
-$(document).ready(load_page())
+function loadPage() {
+    loadRooms();
+    loadTypeServices();
+}
+
+$(document).ready(loadPage())
